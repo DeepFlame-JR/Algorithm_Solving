3a422d720ba346ad15d92417f837f8f0a4209acd
DeepFlame-JR <wnsfuf0121@naver.com>
2022-06-07 16:53:20 +0900

    Scala leet88

---
+++ Scala/src/main/scala/leet88.scala
@@ -0,0 +1,26 @@
+object leet88 extends App{
+  object Solution {
+    def merge(nums1: Array[Int], m: Int, nums2: Array[Int], n: Int): Unit = {
+      var (p1, p2) = (m, n)
+
+      while(p1 > 0 && p2 > 0){
+        if(nums1(p1-1) > nums2(p2-1)){
+          nums1.update(p1+p2-1, nums1(p1-1))
+          p1 -= 1
+        }
+        else{
+          nums1.update(p1+p2-1, nums2(p2-1))
+          p2 -= 1
+        }
+      }
+      // 앞 부분 바꿔줘야 한다.
+      while(p2 > 0){
+        nums1.update(p2-1, nums2(p2-1))
+        p2 -= 1
+      }
+    }
+  }
+
+  Solution.merge(Array(1,2,3,0,0,0), 3, Array(2,5,6), 3)
+  Solution.merge(Array(4,5,6,0,0,0), 3, Array(1,2,3), 3)
+}
