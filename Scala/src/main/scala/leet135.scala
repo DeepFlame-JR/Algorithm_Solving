bfeb5b6fdd060aeb452a42a28ddc566985e3e4db
DeepFlame-JR <wnsfuf0121@naver.com>
2022-07-04 21:26:26 +0900

    Scala leet135

---
+++ Scala/src/main/scala/leet135.scala
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/candy/
+
+object leet135 extends App {
+  object Solution {
+    def candy(ratings: Array[Int]): Int = {
+      val N = ratings.length
+      var candy_arr = Array.fill(N){1}
+
+      def calculate(ratings:Array[Int]): Unit = {
+        var stack = 1
+        for(i <- 1 until N){
+          if(ratings(i) > ratings(i-1)){
+            stack += 1
+            candy_arr(i) = math.max(candy_arr(i), stack)
+          }
+          else{
+            stack = 1
+          }
+        }
+      }
+
+      // 주변(neighbors)보다 사탕을 많이 받아야한다
+      // left > right
+      calculate(ratings)
+
+      // right > left
+      candy_arr = candy_arr.reverse
+      calculate(ratings.reverse)
+
+      candy_arr.sum
+    }
+  }
+  println(Solution.candy(Array(1,0,2))) // 4
+  println(Solution.candy(Array(1,2,2))) // 5
+  println(Solution.candy(Array(1,2,87,87,87,2,1))) // 13
+  println(Solution.candy(Array(1,3,2,2,1))) // 7
+}
