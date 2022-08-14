b7ee0a14d71b84e5d6bb2ff8dee9374602b897f7
JunRyeol <wnsfuf0121@naver.com>
2022-08-14 19:42:40 +0900

    move leet307

--- Scala/src/main/scala/ARRAY/leet307.scala
+++ Scala/src/main/scala/ARRAY/leet307.scala
@@ -1,4 +1,4 @@
-
+package ARRAY
 
 // https://leetcode.com/problems/range-sum-query-mutable/
 
@@ -18,4 +18,17 @@ object leet307 extends App{
     // Memory Limit Exceeded 피하기
     def sumRange(left: Int, right: Int): Int = {
       if(right-left < len/2){ // 길이가 비교적 짧다
-        return arr.slice(lef
+        return arr.slice(left, right+1).sum
+      }else{ // 길이가 비교적 길다
+        val rest = arr.take(left).sum + arr.drop(right+1).sum
+        return s - rest
+      }
+    }
+  }
+
+  val numArray = new NumArray(Array(1,3,5))
+  println(numArray.sumRange(0,2))
+  numArray.update(1,2)
+  println(numArray.sumRange(0,2))
+
+}
